62bef708b604df5346096015f07f11ba91571f1d
SIPHAMANDLAMLANJANA <114500540+Siphamandla201@users.noreply.github.com>
2023-03-02 12:40:09 +0200

    submit

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use((req, res, next)=> {
     next();
 });
 
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:8081'
+}));
+
 const { errorHandling } = require("./middleware/ErrorHandling");
 
 
